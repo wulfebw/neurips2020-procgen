338a2713134e5051ce34d443a2789721be7d2a1e
Blake Wulfe <blake.w.wulfe@gmail.com>
2020-07-06 10:35:42 -0700

    submitting data aug check

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ set -e
 # Your experiment file for submission   #
 #########################################
 
-export EXPERIMENT_DEFAULT="experiments/episode_adversarial_generalization.yaml"
+export EXPERIMENT_DEFAULT="experiments/data_aug_ppo.yaml"
 export EXPERIMENT=${EXPERIMENT:-$EXPERIMENT_DEFAULT}
 
 if [[ -z $AICROWD_IS_GRADING ]]; then
